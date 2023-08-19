66e09d342af0300aa2b1518c0a02603a79ea563a
Al <albarrentine@gmail.com>
2023-08-19 02:46:25 -0400

    [unicode_regexes] adding common python functions

---
+++ unicode_regexes/__init__.py
@@ -0,0 +1,50 @@
+import re
+
+__version__ = "0.0.0"
+
+
+def wide_unicode_char_literal(c):
+    return (
+        "\\u{}".format(c.lower())
+        if len(c) < 5
+        else "\\U{}".format(c.lower().rjust(8, "0"))
+    )
+
+def regex_char_range(match):
+    r = match.split("..")
+    # Wide version
+    return "-".join(
+        [
+            wide_unicode_char_literal(c)
+            for c in r
+        ]
+    )
+
+
+def regex_multi_chars(match):
+    r = match.split(" ")
+    return "".join([
+        "(?:",
+        "".join(
+        [
+            wide_unicode_char_literal(c)
+            for c in r
+        ]),
+        ")"
+    ])
+
+
+
+def regex_char_patterns(text, *regexes):
+    char_patterns = []
+    for line in text.split("\n"):
+        for regex in regexes:
+            m = regex.match(line)
+            if not m:
+                continue
+            chars = m.group(1).strip()
+            if " " not in chars:
+                char_patterns.append(regex_char_range(chars))
+            else:
+                char_patterns.append(regex_multi_chars(chars))
+    return char_patterns
